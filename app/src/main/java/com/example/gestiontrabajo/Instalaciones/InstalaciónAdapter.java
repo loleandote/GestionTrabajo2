f1db973ae3d09bc5494ad822bd70b82316cc0695
loleandote <alfosimo1999@gmail.com>
2021-03-10 01:05:32 +0100

    prueba

---
+++ app/src/main/java/com/example/gestiontrabajo/Instalaciones/InstalaciónAdapter.java
@@ -0,0 +1,85 @@
+package com.example.gestiontrabajo.Instalaciones;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.gestiontrabajo.Datos.Instalación;
+import com.example.gestiontrabajo.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class InstalaciónAdapter extends RecyclerView.Adapter<InstalaciónAdapter.MiViewHolder>{
+    ArrayList<Instalación> lista;
+    private Context context;
+    private View.OnClickListener onClickListener;
+
+    public InstalaciónAdapter(Context context) {
+        lista= new ArrayList<>();
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MiViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i){
+        LayoutInflater inflater =LayoutInflater.from(viewGroup.getContext());
+        System.out.println("hola");
+        View view=inflater.inflate(R.layout.elementos_lista_instalacion,viewGroup,false);
+        view.setOnClickListener(this.onClickListener);
+        MiViewHolder miViewHolder= new MiViewHolder(view);
+        return miViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MiViewHolder holder, int position) {
+        Picasso.get().load(lista.get(position).getImagenes().get(0))
+                .placeholder(R.drawable.icons8_squats_30)
+                .error(R.drawable.icons8_error_cloud_48)
+                //.resize(80,60)
+                //.centerCrop()
+                .into(holder.imagenView);
+        String texto = String.valueOf(lista.get(position).getId());
+        //Integer nombre = lista.get(postion).getId_reserva();
+        //holder.nombreTextView.setText(texto);
+        holder.nombreTextView.setText(lista.get(position).getNombre());
+        String tipo = String.valueOf(lista.get(position).getTipo());
+        holder.tipoTextView.setText(tipo);
+    }
+
+    public void setOnItemClickListener(View.OnClickListener onClickListener)
+    {
+        this.onClickListener= onClickListener;
+    }
+    @Override
+    public int getItemCount() {
+        if(lista==null){
+            return 0;
+        }else
+            return lista.size();
+    }
+    public void anyadirALista(ArrayList<Instalación> lista){
+        this.lista.addAll(lista);
+        notifyDataSetChanged(); // Actualizamos el recyclerView
+    }
+
+
+    public static class MiViewHolder extends  RecyclerView.ViewHolder{
+        public TextView nombreTextView;
+        public TextView tipoTextView;
+        public ImageView imagenView;
+        public MiViewHolder(View view){
+            super(view);
+            this.imagenView =itemView.findViewById(R.id.ImagenInstalacion);
+            this.nombreTextView =itemView.findViewById(R.id.nombreInstalacionTextView);
+            this.tipoTextView =itemView.findViewById(R.id.tipoInstalacionTextView);
+        }
+    }
+}
