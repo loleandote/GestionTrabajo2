6a68002f09918b628bffbe9ac5f47ce647282a69
loleandote <alfosimo1999@gmail.com>
2021-05-26 02:50:59 +0200

    Funciona las reservas 2314

--- app/src/main/java/com/example/gestiontrabajo/ActividadConUsuario.java
+++ app/src/main/java/com/example/gestiontrabajo/ActividadConUsuario.java
@@ -121,6 +121,7 @@ public class ActividadConUsuario extends AppCompatActivity {
                     drawerLayout.openDrawer(GravityCompat.START);
                 }
         }
+        //iugkb
         return super.onOptionsItemSelected(item);
     }
 
