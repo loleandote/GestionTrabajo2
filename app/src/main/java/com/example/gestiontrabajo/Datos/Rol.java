f1c04c5629a96aa59e94112d0724c54334e3adff
loleandote <48246750+loleandote@users.noreply.github.com>
2021-06-11 08:11:09 +0200

    hola rol

--- app/src/main/java/com/example/gestiontrabajo/Datos/Rol.java
+++ app/src/main/java/com/example/gestiontrabajo/Datos/Rol.java
@@ -62,6 +62,7 @@ public class Rol {
         this.mod_usuario_otros = mod_usuario_otros;
         this.mod_permiso = mod_permiso;
         this.exporta_importa = exporta_importa;
+        String hola="hoa";
     }
 
     public boolean isRealiza_reservas() {
